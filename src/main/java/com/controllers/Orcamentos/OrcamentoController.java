f51acc16026216332fe3bba909cecfe8a265029c
Parola <parolajc@hotmail.com>
2021-12-11 16:10:57 +0000

    add Orcamento's controllers and pages

---
+++ src/main/java/com/controllers/Orcamentos/OrcamentoController.java
@@ -0,0 +1,7 @@
+package com.controllers.Orcamentos;
+
+public class OrcamentoController {
+    public void addOrcamentoProgramado(){
+
+    }
+}
--- src/main/java/com/controllers/Orcamentos/OrcamentosController.java
+++ src/main/java/com/controllers/Orcamentos/OrcamentosController.java
@@ -1,4 +1,36 @@
 package com.controllers.Orcamentos;
 
+import com.controllers.PedidosDeOrcamento.PedidosDeOrcamentoController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class OrcamentosController {
+
+    public void adicionarOrcamentoProgramado(ActionEvent actionEvent) {
+        showModal("/view/orcamentos/adicionarOrcamentoProgramado.fxml","Centro de Reparações");
+    }
+
+    public void adicionarOrcamentoFixo(ActionEvent actionEvent) {
+        showModal("/view/orcamentos/adicionarOrcamentoProgramado.fxml","Centro de Reparações");
+    }
+
+    private void showModal(String fxmlName,String title){
+        try {
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            FXMLLoader loader = new FXMLLoader(PedidosDeOrcamentoController.class.getResource(fxmlName));
+            Scene scene = new Scene(loader.load());
+            scene.getStylesheets().add(String.valueOf(PedidosDeOrcamentoController.class.getResource("/css/style.css")));
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+
+        }
+    }
 }
